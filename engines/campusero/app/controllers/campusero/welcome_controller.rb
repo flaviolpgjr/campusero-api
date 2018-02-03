c77cb3687ba3cad67c8105690189b1146f887819
Flavio Junior <flpgjr@gmail.com>
2018-02-03 00:10:55 -0200

    Fixed name user

--- engines/campusero/app/controllers/campusero/welcome_controller.rb
+++ engines/campusero/app/controllers/campusero/welcome_controller.rb
@@ -3,12 +3,12 @@ require_dependency "campusero/application_controller"
 module Campusero
   class WelcomeController < ApplicationController
     def index
-      token = params[:at]
+      @token = params[:at]
       response = HTTParty.get('https://sandboxapi.campuse.ro/user/myprofile/')
       
       
-     headers= {'Authorization': "Bearer #{token}"}
-     
+     headers= {'Authorization': "Bearer #{@token}"}
+      
       @user = HTTParty.get('https://sandboxapi.campuse.ro/user/myprofile/', headers: headers)
       Rails.logger.info @user
      end
