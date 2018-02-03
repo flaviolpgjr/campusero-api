7c15a848711214616cfaa556cb287aace539d269
Flavio Junior <flpgjr@gmail.com>
2018-02-02 23:12:14 -0200

    exibindo user

--- engines/campusero/app/controllers/campusero/welcome_controller.rb
+++ engines/campusero/app/controllers/campusero/welcome_controller.rb
@@ -3,6 +3,11 @@ require_dependency "campusero/application_controller"
 module Campusero
   class WelcomeController < ApplicationController
     def index
-    end
+      token = params[:at]
+
+      @user = HTTParty.get('https://sandboxapi.campuse.ro/user/myprofile/',
+      :options => { :headers => { 'Content-Type' => 'application/json',
+                                  'Authorization' => "Bearer #{token}" } })   
+     end
   end
 end
