9d1c0538e5f08bc16a679769ebe19ce8922cc930
Flavio Junior <flpgjr@gmail.com>
2018-02-03 01:34:57 -0200

    Test welcome

--- engines/campusero/app/controllers/campusero/welcome_controller.rb
+++ engines/campusero/app/controllers/campusero/welcome_controller.rb
@@ -3,12 +3,14 @@ require_dependency "campusero/application_controller"
 module Campusero
   class WelcomeController < ApplicationController
     def index
+      redirect_to and return if root_path if params[:at].blank? 
+
       @token = params[:at]
       @refresh_token = params[:rt]
       response = HTTParty.get('https://sandboxapi.campuse.ro/user/myprofile/')
       
       
-     headers= {'Authorization': "Bearer #{@token}"}
+      headers= {'Authorization': "Bearer #{@token}"}
       
       @user = HTTParty.get('https://sandboxapi.campuse.ro/user/myprofile/', headers: headers)
       Rails.logger.info @user
