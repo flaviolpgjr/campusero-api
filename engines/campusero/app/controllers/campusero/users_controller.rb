cd432b023f7c36b02edf4ee6cedc488da5f60c53
Flavio Junior <flpgjr@gmail.com>
2018-02-03 05:06:48 -0200

    Variaveis de amviente

--- engines/campusero/app/controllers/campusero/users_controller.rb
+++ engines/campusero/app/controllers/campusero/users_controller.rb
@@ -3,6 +3,8 @@ require 'json'
 
 module Campusero
   class UsersController < ApplicationController
+    include HTTParty
+    base_uri 'https://#{ENV['CPBR_CLIENT_ID']}:#{ENV['CPBR_SECRET_KEY']}@sandboxaccounts.campuse.ro'
 
     # GET campusero/code?code=:code
     def callback
@@ -15,7 +17,9 @@ module Campusero
             "redirect_uri": "https://campusero-api.herokuapp.com/campusero/code"
           }
         }
-        response = HTTParty.post("https://JsZVPD3fEmZdE9HElgng3Y6Zxxpwi8kMh6g2OTMO:YXW4gvViztWpnK2XSbcSCI9vnvdMo5vO9FfRsOhexh6xHUaJUPTTzHFKvhDtMDXA85XAuC7TKXuLvyLSCGJ2i0ej8LEfiHZVsUHDaDXtgK89F3i7dMbZAHX8aaaulLs1@sandboxaccounts.campuse.ro/o/token/",payload)
+        response = self.class.post("/o/token/",payload)
+        Rails.logger.info "********************"
+        Rails.logger.info response
         
         redirect_to welcome_index_path({at:response['access_token'],rt: response['refresh_token']})
       end
